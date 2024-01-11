4faab9090cb305acb71a01984593182e2ec21158
leonelborgesdev <leonel03t@gmail.com>
2024-01-10 20:09:32 -0400

    main partyGuestList

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,17 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Intro with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        PartyGuestList partyGuestList=new PartyGuestList();
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        partyGuestList.addGuest("Juan");
+        partyGuestList.addGuest("Maria");
+        partyGuestList.addGuest("Pedro");
 
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.println("¿Esta Juan en la lista de invitados?:"+partyGuestList.isGuestList("Juan"));
+        System.out.println("¿Esta Ana en la lista de invitados:?"+partyGuestList.isGuestList("Ana"));
+
+        partyGuestList.removeGuest("Maria");
+        System.out.println("Numero total de invitados:"+partyGuestList.getTotalGuest());
+        System.out.println("¿La lista de invitados esta vacia?:"+partyGuestList.isGuestListEmpty());
     }
 }
